01f122fc2359496c67728c8f277ffebe183e7b5a
X104n <github.freeware171@passmail.net>
2022-05-05 17:46:11 +0200

    Choose wisely

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { SkeBasis } from '@skatteetaten/frontend-components/SkeBasis';
+import { Button } from '@skatteetaten/frontend-components/Button';
+import {Quiz} from './Quiz';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SkeBasis>
+      <Quiz></Quiz>
+      {/* <div className="App-header">
+        Hei
+        <Button buttonStyle="primary" mobileFullWidth>
+          Send inn skjema
+        </Button>
+      </div> */}
+
+    </SkeBasis>
   );
 }
 
